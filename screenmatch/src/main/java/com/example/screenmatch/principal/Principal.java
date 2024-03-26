d1e3f5f80f217364b96d81f6decce649ccd02a38
Marcos <marcosvictor82@gmail.com>
2024-03-26 14:40:46 -0300

    criando classe de episódio e tratando entrada para prevenção de erros

--- screenmatch/src/main/java/com/example/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/example/screenmatch/principal/Principal.java
@@ -1,11 +1,15 @@
 package com.example.screenmatch.principal;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import com.example.screenmatch.model.DadosEpisodio;
 import com.example.screenmatch.model.DadosSerie;
 import com.example.screenmatch.model.DadosTemporada;
+import com.example.screenmatch.model.Episodio;
 import com.example.screenmatch.service.ConsumoApi;
 import com.example.screenmatch.service.ConverteDados;
 
@@ -34,6 +38,35 @@ public class Principal {
 			DadosTemporada dadosTemporada= conversor.obterDados(json,DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		    };
-		    temporadas.forEach(System.out::println);
+		    // temporadas.forEach(System.out::println);
+            // for (int i = 0;i<dados.totaltemporadas();i++){
+            //     List<DadosEpisodio> episodiosTemporada=temporadas.get(i).episodios();
+            //     for (int j=0;j<episodiosTemporada.size();j++){
+            //         System.out.println(episodiosTemporada.get(j).titulo());
+            //     }
+            // }
+            //funções lambidas cada vez que for interar em uma coleção
+            temporadas.forEach(t->t.episodios().forEach(e-> System.out.println(e.titulo())));
+            List<DadosEpisodio> dadosEpisodios=temporadas.stream()
+            .flatMap(t->t.episodios().stream())
+            .collect(Collectors.toList());
+
+            System.out.println("\n Top 5 episódios");
+            dadosEpisodios.stream()
+            .filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
+            .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+            .limit(5)
+            .forEach(System.out::println);
+
+            
+            // List<String> nomes = Arrays.asList("Jacque","Iasmin","Paulo","Rodrigo");
+            // nomes.stream().sorted().filter(n->n.startsWith("P")).map(n->n.toUpperCase()).limit(3).forEach(System.out::println);
+            
+            List<Episodio> episodios=temporadas.stream()
+            .flatMap(t->t.episodios().stream()
+            .map(d-> new Episodio(t.numero(),d))
+            ).collect(Collectors.toList());
+
+            episodios.forEach(System.out::println);
         }
 }
